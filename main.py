264a734c095b11ec307b3ed9ac0ed614faecbdf5
CharlesM-WEB <ctmarchant11@gmail.com>
2025-02-20 21:15:41 -0500

    first wordle

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ response = client.models.generate_content(
     model="gemini-2.0-flash",
     contents="give me a random five letter word in lower case letters",
 )
-
-
+#variables
 word = response.text 
+GREEN = "\033[42m"
+RESET = "\033[49m"
+YELLOW = "\033[103m"
+
+
+print ("give me a five letter word ")
+guess = input ()
+
+while len(guess) < 5: 
+    guess += "_"
+    
+question = "Is "+guess+" a five letter word in the english dictionary? (respond yes if so or no if not)"
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=question,
+).text.lower()
+
+print (response)
