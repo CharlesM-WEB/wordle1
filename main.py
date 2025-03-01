0b0dc9adeb2eda37805f1af6e53219c5ea75d0f6
CharlesM-WEB <ctmarchant11@gmail.com>
2025-02-28 19:06:15 -0500

    finish

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 from google import genai
+import random
 
 client = genai.Client(api_key="AIzaSyDRpy7nuaUNwfpsXf9JPoBvR0471-XcNZU")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="give me a random five letter word in lower case letters",
+    contents="give me just a list of 20 random five letter words with no explanation or punctuation."
 )
+ 
+wordList = response.text.splitlines()
+word = random.choice(wordList)
+ 
 #variables
-word = response.text 
 GREEN = "\033[42m"
 RESET = "\033[49m"
 YELLOW = "\033[103m"
 
+for i in range(5):
+    while True:
+        print ("give me a five letter word ")
+        guess = input ()[0:5]
 
-print ("give me a five letter word ")
-guess = input ()
+        while len(guess) < 5: 
+            guess += "!"
+        
+        question = "Is "+guess+" a five letter word in the Miriam-Webster English Dictionary? (yes or no)"
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=question,
+        ).text.lower()
 
-while len(guess) < 5: 
-    guess += "_"
-    
-question = "Is "+guess+" a five letter word in the english dictionary? (respond yes if so or no if not)"
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=question,
-).text.lower()
+        if "yes" in response:
+            break
 
-print (response)
+    result = ""
+    for i in range(5):
+        letter = guess[i]
+        if word[i] == letter:
+            result += GREEN + letter + RESET
+        elif letter in word:
+            result += YELLOW + letter + RESET 
+        else: 
+            result += letter 
+    print(result)
